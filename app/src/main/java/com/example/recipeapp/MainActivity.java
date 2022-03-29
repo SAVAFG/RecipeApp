490609944c962217f54f32c23a9ad7387020fd4e
sdaga56 <srdaga@wpi.edu>
2022-03-29 11:05:39 -0400

    initial push - saaya

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hello - from Aaron");
+        System.out.println("Hello - from Saaya");
     }
 }
