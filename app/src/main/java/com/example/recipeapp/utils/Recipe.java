eecb630ca71a311993a004632bb16d7c8a57b4f9
WaiWasabi <aarontianqwerty@outlook.com>
2022-04-12 10:22:08 -0400

    added firebase and recipe class

--- app/src/main/java/com/example/recipeapp/utils/Recipe.java
+++ app/src/main/java/com/example/recipeapp/utils/Recipe.java
@@ -1,7 +1,39 @@
 package com.example.recipeapp.utils;
 
-public class Recipe {
-    public Recipe(){
+import java.util.ArrayList;
 
+public class Recipe {
+    String name;
+    ArrayList<String> ingredients;
+    ArrayList<String> directions;
+
+    public Recipe(String name, ArrayList<String> ingredients, ArrayList<String> directions){
+        this.name = name;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(ArrayList<String> directions) {
+        this.directions = directions;
     }
 }
