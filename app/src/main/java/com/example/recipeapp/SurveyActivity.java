cc2bf1ea13387fed36613fc4f47028c5b1933839
VInayak Rao <vinayakr2005@gmail.com>
2022-04-12 10:27:48 -0400

    Survey Interface (v1) April 12, 10:26 AM

--- app/src/main/java/com/example/recipeapp/SurveyActivity.java
+++ app/src/main/java/com/example/recipeapp/SurveyActivity.java
@@ -11,7 +11,7 @@ public class SurveyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.survey_page);
+        setContentView(R.layout.activity_survey);
     }
 
     public void checkOutput(View v) {
