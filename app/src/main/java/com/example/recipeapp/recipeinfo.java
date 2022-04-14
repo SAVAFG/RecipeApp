bfbff92809c87d7d4d21a53c286c0bfff9d43bda
sdaga56 <srdaga@wpi.edu>
2022-04-13 20:11:41 -0400

    saaya push 1

--- app/src/main/java/com/example/recipeapp/recipeinfo.java
+++ app/src/main/java/com/example/recipeapp/recipeinfo.java
@@ -19,8 +19,8 @@ public class recipeinfo extends AppCompatActivity {
 
 
     String[] s1;
-    int[] images = {R.drawable.food2, R.drawable.food3,R.drawable.food4,R.drawable.food5,
-            R.drawable.food6,R.drawable.food7};
+    int[] images = {R.drawable.food2, R.drawable.food3, R.drawable.food4, R.drawable.food5,
+            R.drawable.food6, R.drawable.food7};
     EditText txt_Search;
 
     @Override
@@ -32,43 +32,11 @@ public class recipeinfo extends AppCompatActivity {
 
         s1 = getResources().getStringArray(R.array.recipenamesexample);
 
-        txt_Search=(EditText)findViewById(R.id.txt_searchtext);
 
         recycleadaptor myadaptor = new recycleadaptor(this, s1, images);
 
         recyclerView.setAdapter(myadaptor);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        txt_Search.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                filter(s.toString());
-            }
-        });
-
     }
-
-        private void filter(String text) {
-            ArrayList<RecipeDataset> filterList = new ArrayList<>();
-                //figure out what array list i want to look through
-            //for (RecipeDataset item: s1) {
-              //  if (item.getItemName().toLowerCase().contains(text.toLowerCase())) {
-
-                //    filterList.add(item);
-
-                }
-       //     }
-
-
-     //   }
 }
